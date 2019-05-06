90bf08ddc89ad6c4270273d88b13671c5ad4bb28
Kostya Shabalev <kvshabalev@gmail.com>
2019-05-06 15:55:56 +0300

    evening commit

--- controllers/petShop.controller.js
+++ controllers/petShop.controller.js
@@ -2,18 +2,76 @@ import { PetShopModel } from "../models/petShop.model.js";
 import { PetShopView } from "../views/petShop.view.js";
 
 export class PetShopController {
+
+  get pets() {
+
+    return this.petShopModel.petStore;
+  }
+
 	constructor(petShopElement) {
 		this.petShopModel = new PetShopModel();
 		this.petShopView = new PetShopView(petShopElement);
 	}
 
 	initiatePetShop() {
-		let petsStoringPromise = this.petShopModel.storePets();
+		let petsStoringPromise = this.petShopModel.getAnimals();
 
 		petsStoringPromise
-			.then(categories => {
-				this.renderCategories(categories);
+			.then(animals => {
+        this.storeAnimals(animals);
+        this.fillTopCategories();
+        this.renderCategories(this.petShopModel.categories);
 			});
+	}
+
+	storeAnimals(animals) {
+    this.petShopModel.sortRecievedAnimals(animals);
+  }
+  
+  fillTopCategories() {
+    const topCategories = this.petShopModel.categories.topCategories;
+
+    for (let category in topCategories) {
+			topCategories[category] = this.fillTopCategory(category);
+		}
+  }
+
+  fillTopCategory(category) {
+    switch (category) {
+			case 'cats':
+
+				return this.petShopModel.categories.mainCategories[category];
+			case 'expensive':
+
+				return this.getExpensivePets();
+		}
+  }
+
+  getExpensivePets() {
+    const pets = this.pets;
+		const averagePrice = this.getAveragePrice(pets);
+		const higherThanAverage = pets.filter(pet => {
+			
+			return pet.price > averagePrice;
+		});
+
+		return higherThanAverage;
+	}
+
+	getAveragePrice(pets) {
+		const totalPrice = this.getTotalPrice(pets);
+		
+		return totalPrice / pets.length;
+	}
+	
+	getTotalPrice(pets) {
+		let totalPrice = 0;
+
+		pets.forEach( item => {
+			totalPrice += item.price;
+		});
+
+		return totalPrice;
 	}
 
 	renderCategories(categories) {
--- models/petShop.model.js
+++ models/petShop.model.js
@@ -24,30 +24,29 @@ export class PetShopModel {
 		};
 	}
 
-	storePets() {
+	getAnimals() {
 		let promise = new Promise((resolve) => {
 
-			this.getAllPets()
+			this.fetchAnimals()
 			.then(response => {
 				return response.json();
 			})
 			.then(data => {
-				this.sortRecievedPets(data);
-				this.fillTopCategories();
-				resolve(this.categories);
+				// this.fillTopCategories();
+				resolve(data);
 			});
 		});
 
 		return promise;
 	}
 
-	getAllPets() {
+	fetchAnimals() {
 		const url = this.requestURL;
 
 		return fetch(url);
 	}
 
-	sortRecievedPets(animals) {
+	sortRecievedAnimals(animals) {
 		let store = this.petStore;
 		let mainCategories = this.categories.mainCategories;
 
@@ -95,66 +94,5 @@ export class PetShopModel {
 
 		return pet;
 	}
-
-	fillTopCategories() {
-		const topCategories = this.categories.topCategories;
-
-		for (let category in topCategories) {
-			topCategories[category] = this.fillTopCategory(category);
-		}
-	}
-
-	fillTopCategory(category) {
-		switch (category) {
-			case 'cats':
-
-				return this.categories.mainCategories[category];
-			case 'expensive':
-
-				return this.getExpensivePets();
-		}
-	}
-
-	getExpensivePets() {
-		const averagePrice = this.getAveragePrice();
-		const higherThanAverage = this.petStore.filter(pet => {
-			
-			return pet.price > averagePrice;
-		});
-
-		return higherThanAverage;
-	}
-
-	getAveragePrice() {
-		const totalPrice = this.getTotalPrice();
-		
-		return totalPrice / this.petStore.length;
-	}
-	
-	getTotalPrice() {
-		let totalPrice = 0;
-
-		this.petStore.forEach( item => {
-			totalPrice += item.price;
-		});
-
-		return totalPrice;
-	}
-
-	// createCategory(category, isAnimalCategory) {
-	// 	if (isAnimalCategory) {
-	// 		this.store.animalCategories[category] = [];
-	// 	} else {
-	// 		this.store.categoriesToShow[category] = [];
-	// 	}
-	// }
-
-	// addPetToCategory(pet, category, isAnimalCategory) {
-	// 	if (isAnimalCategory) {
-	// 		this.store.animalCategories[category].push(pet);
-	// 	} else {
-	// 		this.store.categoriesToShow[category].push(pet);
-	// 	}
-	// }
 
 }
